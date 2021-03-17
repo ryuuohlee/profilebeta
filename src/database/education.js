8baa5fc429885effa6b3e514be04581935b3e7d0
Jeffrey Lee <jglee1@live.com>
2021-03-17 14:36:54 -0700

    Feature: Updated image links on Education to S3 Links

--- src/database/education.js
+++ src/database/education.js
@@ -6,7 +6,7 @@ export const schools = [
     id: edNum += 1,
     school: 'UCBerkeley Extension',
     study: 'Technical Project Management',
-    schoolImg: '../src/images/UC-Berkeley-Extension.png',
+    schoolImg: 'https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/UC-Berkeley-Extension.png',
     location: 'USA',
     duration: '01/2021 - 06/2021',
     description: '500+ hours of intensive development of in-demand technical leadership and business management skills for the Software Development Life Cycle.',
@@ -16,7 +16,7 @@ export const schools = [
     id: edNum += 1,
     school: 'Hack Reactor',
     study: 'Software Engineering, Full-Stack Development',
-    schoolImg: '../src/images/galvanize.png',
+    schoolImg: 'https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/galvanize.png',
     location: 'USA',
     duration: '08/2019 - 11/2019',
     description: '1000+ hours of intensive HTML, CSS, and JavaScript/React study for Full Stack Development with an admission rate of less than 3%.',
@@ -29,28 +29,28 @@ export const certifications = [
   {
     id: certNum += 1,
     school: 'Scrum.org',
-    schoolImg: '../src/images/Scrumorg-Logo_tagline-TM_(1)_(1)_(2).jpg',
+    schoolImg: 'https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/Scrumorg-Logo_tagline-TM_(1)_(1)_(2).jpg',
     location: 'USA',
     achievements: ['Professional Scrum Master I, https://www.credential.net/1535fe5d-9e0f-4158-904d-102d9864f3fe?username=jeffreylee97355#gs.vxg5nt'],
   },
   {
     id: certNum += 1,
     school: 'Codecademy',
-    schoolImg: '../src/images/codecademy.jpg',
+    schoolImg: 'https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/codecademy.jpg',
     location: 'USA',
     achievements: ['Learn Testing for Web Development, https://www.credential.net/19136812-e790-45c6-a014-94c18e49276b?username=jeffreylee97355#gs.hx34p2', 'Create a Front-End App with React, https://www.credential.net/701a8551-8460-4bc2-b7f0-8c8116ae266a?username=jeffreylee97355#gs.hx3a8x'],
   },
   {
     id: certNum += 1,
     school: 'Zero to Mastery',
-    schoolImg: '../src/images/ztm.jpeg',
+    schoolImg: 'https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/ztm.jpeg',
     location: 'USA',
     achievements: ['Mastering the Coding Interview: Data Structures and Algorithms, https://www.credential.net/85da0aa1-0955-4236-acda-a65ea2be838c?username=jeffreylee97355']
   },
   {
     id: certNum += 1,
     school: 'Forage',
-    schoolImg: '../src/images/forage.png',
+    schoolImg: 'https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/forage.png',
     location: 'USA',
     achievements: ['Software Engineering, https://www.credential.net/d077a204-2a74-408f-ae6e-057c4cf50152?username=jeffreylee97355#gs.hx6sjm']
   }
