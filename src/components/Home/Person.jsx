39f97ad405070396b870c4631e1f9455b4b54f3a
Jeffrey Lee <jglee1@live.com>
2021-03-17 13:52:25 -0700

    Feature: Updated portfolio image to S3 location

--- src/components/Home/Person.jsx
+++ src/components/Home/Person.jsx
@@ -4,7 +4,7 @@ const Person = () => {
   return(
     <div className="site-nav-main">
       <div className="person-content">
-          <img className="person-picture" src="../src/images/portfolioportrait.png" />
+          <img className="person-picture" src="https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/portfolioportrait.png" />
           <h1 className="person-title-name">Jeffrey Lee</h1>
             <h2 className="person-subtitle-job-title">Full-Stack Engineer</h2>
       </div>
