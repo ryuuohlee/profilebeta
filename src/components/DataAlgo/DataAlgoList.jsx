df8bc300bafe937eb59c08266bde3c046e2b6a14
Jeffrey Lee <jglee1@live.com>
2021-06-06 23:52:56 -0700

    Feature: setting up coding contest scores

---
+++ src/components/DataAlgo/DataAlgoList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class DataAlgoList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      contests: [],
+      ratings: [],
+      ranks: []
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+};
+
+export default DataAlgoList;
