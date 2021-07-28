feb20c133ed4fadba560df86e00ace17a98e1225
Jeffrey Lee <jglee1@live.com>
2021-07-27 23:40:37 -0700

    Feature: setting up the blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => {
     <div className="blog-body">
       <div className="title">
         <h2 className="section-title">
-          Tech Blog:
+          JeffreyRig
         </h2>
       </div>
       <div className="about-content">
@@ -18,7 +18,7 @@ const Blog = () => {
         </div>
         <div className="about-story">
           <p>
-            Hello! My name is Jeffrey and I enjoy building things that make a difference. My interest in engineering started in the Montgomery Wards Autoshop, watching my dad work on cars. Then, my passion for computers came from my aunt who was the Technologies Director at HUD. I built my first PC at the age of 11 and then took Web Design in 2004 starting high-school career and the rest is history.
+            jeffreyrig.com is my blog where you can follow my journey as an engineer. I will go in-depth into the projects. These projects will be discussing engineering topics and technologies that I find facinating.
           </p>
         </div>
       </div>
